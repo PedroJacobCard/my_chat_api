382b20c355c2600fe847bfcd2fb01604902950f2
Pedro Henrique Jacob Cardoso <pedrojacob05cardoso@gmail.com>
2025-07-30 16:14:15 +0200

    Add CORS acceptances

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   const configService = app.get(ConfigService);
 
   const docConfig = new DocumentBuilder()
