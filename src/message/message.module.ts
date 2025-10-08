9c7515576eb65a3763dd98bf74c57582a3008fc4
Pedro Henrique Jacob Cardoso <pedrojacob05cardoso@gmail.com>
2025-10-08 17:39:51 +0200

    Add Chat modules

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ChatModule } from 'src/chat/chat.module';
 
 @Module({
   providers: [MessageService],
   controllers: [MessageController],
-  imports: [PrismaModule],
+  imports: [PrismaModule, ChatModule],
 })
 export class MessageModule {}
